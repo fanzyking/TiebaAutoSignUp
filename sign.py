09ba8ccad121f4f3beb208549dca0f7516c8f8e4
fanzyking <36314368@qq.com>
2026-06-02 10:19:00 +0800

    Update sign.py

--- sign.py
+++ sign.py
@@ -12,6 +12,9 @@ import hashlib
 # 移动端签到接口
 sign_url = "https://c.tieba.baidu.com/c/c/forum/sign"
 
+# 通知信息
+notice = ""
+
 
 # 单个贴吧签到
 # tieba_name:贴吧名
@@ -36,20 +39,24 @@ def tieba_sign_in(tieba_name, tbs, BDUSS):
         json_resp = resp.json()
         if "user_info" in json_resp:
             logger.debug("签到成功：" + tieba_name + "吧")
+            notice += "签到成功：" + tieba_name + "吧" + '\n'
             return True
         elif json_resp["error_code"] == "160002":
             # 已签到
             logger.error(
                 "签到失败：" + tieba_name + "吧" + " 失败原因：" + json_resp["error_msg"]
             )
+            notice += "签到失败：" + tieba_name + "吧" + "，失败原因：" + json_resp["error_msg"] + '\n'
         else:
             logger.error("签到失败：" + tieba_name + "吧")
             logger.debug(str(json_resp))
             logger.error("失败原因：" + json_resp["error_msg"])
+            notice += "签到失败：" + tieba_name + "吧" + "，失败原因：" + json_resp["error_msg"] + '\n'
     except Exception as e:
         logger.error("签到失败：" + tieba_name + "吧")
         logger.error("报错：" + str(e))
         logger.debug("返回数据：" + resp.text)
+        notice += "签到失败：" + tieba_name + "吧" + "，报错了!!!" + '\n'
         return False
     
     return False
@@ -72,10 +79,28 @@ def sign_in():
     logger.info("共计" + str(sign_sum) + "个贴吧")
     logger.info("成功" + str(sign_sum - faliure_sum) + "个")
     logger.info("失败" + str(faliure_sum) + "个")
+    
+    if "SendKey" in os.environ:
+        notice += "共计" + str(sign_sum) + "个贴吧：成功" + str(sign_sum - faliure_sum) + "个，失败" + str(faliure_sum) + "个。"
+        api = f'https://sc.ftqq.com/{os.environ["SendKey"]}.send'
+        title = u"贴吧签到信息"
+        data = {
+        "text":title,
+        "desp":notice
+        }
+        try:
+            req = requests.post(api, data=data, timeout=60)
+            if req.status_code == 200:
+                logger.info("Server酱通知发送成功")
+            else:
+                logger.info(f"通知失败，状态码：{req.status_code}")
+                logger.info(api)
+        except Exception as e:
+            logger.error(f"通知发送异常：{e}")
 
 
-if __name__ == "__main__":
-    tieba_name = "余额宝"
-    tieba_url = "https://tieba.baidu.com/f?kw=%D3%E0%B6%EE%B1%A6"
-    logger.set_logger("debug")
-    tieba_sign_in(tieba_name, tieba_url)
+# if __name__ == "__main__":
+#     tieba_name = "余额宝"
+#     tieba_url = "https://tieba.baidu.com/f?kw=%D3%E0%B6%EE%B1%A6"
+#     logger.set_logger("debug")
+#     tieba_sign_in(tieba_name, tieba_url)
